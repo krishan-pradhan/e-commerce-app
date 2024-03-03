74b5154a2a6261863aec53f0b3b781d631a6d8d7
Krishan Pradhan <krishan@quadranttech.co>
2024-03-03 19:56:39 +0530

    error page

--- app/stripe/error/page.tsx
+++ app/stripe/error/page.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <>
+      <section className="container">
+        <div className='flex h-[100dvh] flex-col items-center justify-center gap-5'>
+          <h1 className='text-red-500 lg:text-4xl'>Payment Decline</h1>
+          <Button type='button' variant={'link'} >
+            <Link className='underline' href={'/'}>Go back to home </Link>
+          </Button>
+        </div>
+      </section>
+    </>
   )
 }
 
--- app/stripe/success/page.tsx
+++ app/stripe/success/page.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 const SuccessPage = () => {
   return (
-    <div>SuccessPage</div>
+    <section className="container">
+    <div className='flex h-[100dvh] flex-col items-center justify-center gap-5'>
+      <h1 className='text-green-500 lg:text-4xl'>Payment Success</h1>
+      <Button type='button' variant={'link'} >
+        <Link className='underline' href={'/'}>Go back to home </Link>
+      </Button>
+    </div>
+  </section>
   )
 }
 
