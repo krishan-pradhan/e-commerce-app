02fb7ad096206256a20cda60c40bc124cf257446
Krishan Pradhan <krishan@quadranttech.co>
2024-03-03 21:46:05 +0530

    test

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions = {
+ const authOptions = {
     secret: process.env.NEXTAUTH_SECRET,
     providers: [
         GoogleProvider({
