5af2f101d903d21133caf51add3836212fb0f903
Krishan Pradhan <krishan@quadranttech.co>
2024-03-05 21:11:16 +0530

    responsive nav

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -17,9 +17,10 @@ const NavBar = () => {
     const [menuOpen, setMenuOpen] = useState(false); 
   return (
     <>
-    <nav id="mainNav" className={`lg:p-4 py-2 sticky ${menuOpen? "active": ""}`}>
-        <div className="container mx-auto flex justify-between lg:items-center item-start lg:flex-row flex-col overflow-y-auto">
-          <div className="flex items-center justify-between" >
+    <header id="mainHeader" className={`fixed top-0 left-0 w-full backdrop-blur-2xl z-40 mainHeader ${menuOpen? "active": ""}`}>
+      <div id="mainNav" className={`inner-header lg:p-4 py-2 sticky ${menuOpen? "active": ""}`}>
+        <div className="header-wrapper container mx-auto flex lg:justify-between justify-start lg:items-center item-start lg:flex-row flex-col overflow-y-auto">
+          <div className="flex items-center justify-between">
           <Link className="border p-3 rounded-full hover:opacity-75" title="store logo" href="/"><Store/></Link>
             <div className="flex justify-center items-center sm:gap-5 gap-1">
               {session? <p className=" text-xs sm:text-sm lg:hidden block">{session?.user?.name}</p>: '' }
@@ -30,17 +31,19 @@ const NavBar = () => {
               </Toggle>
             </div>
           </div>
-          <div className="menu-list flex lg:items-center items-start gap-5 lg:min-w-[60%] justify-between lg:flex-row flex-col ">
-              <ul className={`flex lg:space-x-4 lg:flex-row flex-col lg:w-auto w-full items-center ${menuOpen? 'menuOpen': 'menuclose'}`}>
-                  {mainNavLink.map((link, i) =>(
-                      <li className="{}" key={i}>
-                      <Link href={link.href} onClick={()=> setMenuOpen(menuOpen ? false : menuOpen)}>
-                        <span className={`lg:text-base text-2xl lg:py-0 py-5 block hover:opacity-70 ${pathName === link.href ? " opacity-70": 'hover:underline'}`}>{link.name}</span>
-                      </Link>
-                    </li>
-                  ) 
-                  )}
+          <div className="menu-list flex lg:items-center items-start gap-5 lg:min-w-[60%] justify-between lg:flex-row flex-col">
+              <nav> 
+                <ul className={`flex lg:space-x-4 lg:flex-row flex-col lg:w-auto w-full items-center ${menuOpen? 'menuOpen': 'menuclose'}`}>
+                    {mainNavLink.map((link, i) =>(
+                        <li className="{}" key={i}>
+                        <Link href={link.href} onClick={()=> setMenuOpen(menuOpen ? false : menuOpen)}>
+                          <span className={`lg:text-base text-2xl lg:py-0 py-5 block hover:opacity-70 ${pathName === link.href ? " opacity-70": 'hover:underline'}`}>{link.name}</span>
+                        </Link>
+                      </li>
+                    ) 
+                    )}
               </ul>
+              </nav>
               <div className={`flex items-center lg:justify-start justify-center gap-2 lg:w-auto w-full ${menuOpen? 'menuOpen': 'menuclose'}`}>
                 <Link href={'/search'}><Button tabIndex={-1} variant={"ghost"}><Search size={"18px"}/></Button></Link>
                 <Button variant={"ghost"} color={"black"} onClick={()=>{session? handleCartClick(): signIn()} }><ShoppingBag size={"18px"}/></Button>
@@ -50,7 +53,8 @@ const NavBar = () => {
               </div>
           </div>
         </div>
-    </nav>
+      </div>
+      </header>
     </>
   )
 }
